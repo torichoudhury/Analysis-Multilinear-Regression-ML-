678c215abe44bdd3d6f2260c622315ac31892273
torichoudhury <torichoudhury@gmail.com>
2023-11-15 14:22:13 +0530

    Final edit

--- mini_project.py
+++ mini_project.py
@@ -1,3 +1,17 @@
+"""The following block of code works on 4 primary functions 
+1. Preprocess(dataset,path): This function is to check for any missing values and replacing it with the mean of the column
+2. Visualising(xaxis,yaxis,data): It plots the graph based on the the name of the column provided
+3. Regressionmodel(dataset): This is a Machine Learning model for predicting Active Power by learning the patterns from existing dataset
+    Multi-Linear Regression model is used. 
+    For demonstration Dataset of Independent variable is taken from the existing dataset by splitting it.
+4. main(): All the above functions can be callede by user under this function 
+
+Note: The sample csv 'Sample(2023)h.csv' has missing values in Vrms and Irms column given to demonstrate Preprocess() function,
+and hence the others functions won't work until Preprocess() function is run atleast once.
+
+Thank you.
+"""
+
 #importing modules
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,15 +20,12 @@ from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-"""Data Preprocessing 
-1) Import the dataset (using pandas library).
-2) Check for missing values in the dataset and handle them appropriately.
 
-"""
+#to preprocess data by substituting the missing values 
 def preprocess(dataset,path):
-    #importing dataset from csv 
     X = dataset.iloc[:, :-1].values
     y = dataset.iloc[:, -1].values
+
     #taking care of missing values 
     imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
     imputer.fit(X[:, 1:3])
@@ -25,59 +36,80 @@ def preprocess(dataset,path):
     processed.to_csv(path, index=False)
     
 
-
-
-
-#visualising data in form of graphs(user preferanced)
+#visualising data in form of graphs
 def visualising(xaxis,yaxis,data):
     xlist=[]
     ylist=[]
     for index, row in data.iterrows():
         xlist.append(row[xaxis])
         ylist.append(row[yaxis])
+    #plotting graphs 
     plt.plot(xlist, ylist)
     plt.title(" ")
+    plt.xlabel(xaxis)
+    plt.xticks(rotation=45)
+    plt.ylabel(yaxis)
+    plt.grid(True)
+    plt.legend()
     plt.show()
 
 def regressionmodel(dataset):
+    print("\nThis regression model can predict the value of 'Active Power' by analysing the 'Vrms','Irms and 'Frequency'")
+    print("\nTo demonstrate the model, the dataset is split into Training set and Test set")
+    print("The model is trained on the training set and used on the test set and the output is compared with actual values")
     X = dataset.iloc[:, 1:-1].values
     y = dataset.iloc[:, -1].values
-    #splitting data into training set and test set
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-    #training model on the training set 
-    
+    # Train-Test Split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    print("\nTest set of Independent variables(Vrms,Irms and Frequency respectively)","\n",X_test,"\n","Actual values of Active power corresponding to Test set \n",y_test)
+
+    # Linear Regression Model Training
     regressor = LinearRegression()
     regressor.fit(X_train, y_train)
-    #predicting the Test set results
+
+    # Prediction
     y_pred = regressor.predict(X_test)
-    np.set_printoptions(precision=2)
-    print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+    print("\nPredicted values of Active power corresponding to the test set\n",y_pred,"\n")
+    
+    try:
+        wish=int(input("Enter any number to visualize the comparison on a graph "))
+        # Plotting the predicted vs. actual values
+        plt.scatter(y_test, y_pred, color='green')
+        plt.title('Active Power')
+        plt.xlabel('Actual Values')
+        plt.ylabel('Predicted Values')
+        plt.show()
+    except ValueError:
+        print("Returning to main menu")
 
-
+    
 #main function 
 def main():
     data = pd.read_csv("Sample(2023)h.csv")
     desdata = "Processed.csv"
+    
     while True:
         print("\nChoose the number to perform an action\n1-Data Pre-processing.\n2-Visualize Graphs.\n3-Predicting data\n4-Exit.")
-        choose=int(input("Enter your choice: "))
-        #try:
-        if choose==1:
-            preprocess(data,desdata)
-        elif choose==2:
-            xaxis=input("Enter the name of the first axis variable :")
-            yaxis=input("Enter the name of the second axis variable :")
-            visualising(xaxis,yaxis,pd.read_csv(desdata))
-        elif choose==3:
-            regressionmodel(pd.read_csv(desdata))
-        elif choose==4:
-            exit()
-        else:
-            print("Please Enter a Valid Number")
+        print("Please note that 2 and 3 does not work until 1 is used atleast once. \n ")
+
+        try:
+            choose=int(input("Enter your choice: "))
+            if choose==1:
+                preprocess(data,desdata)
+            elif choose==2:
+                xaxis=input("Enter the name of the first axis variable :")
+                yaxis=input("Enter the name of the second axis variable :")
+                visualising(xaxis,yaxis,pd.read_csv(desdata))
+            elif choose==3:
+                regressionmodel(pd.read_csv(desdata))
+            elif choose==4:
+                exit()
+            else:
+                print("Please Enter a Valid Number")
+                continue
+        except ValueError:
+            print("Please Enter a Valid input and try again ")
             continue
-        #except ValueError:
-            #eak
 main()
             
